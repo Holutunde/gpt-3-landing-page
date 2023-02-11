3dd63e4597dde4cb7415b7ae46a5d42f02283d2f
Holutunde <Holutunde99@gmail.com>
2023-02-11 04:25:18 +0100

    added feature component

--- src/component/feature/Feature.jsx
+++ src/component/feature/Feature.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import './feature.css'
+import React from 'react';
+import './feature.css';
 
-const Feature = () => {
-  return (
-    <div>
-      
+const Feature = ({ title, text }) => (
+  <div className="feature_container">
+    <div className="title">
+      <h1>{title}</h1>
     </div>
-  )
-}
+    <div className="text">
+      <p>{text}</p>
+    </div>
+  </div>
+);
 
-export default Feature
+export default Feature;
