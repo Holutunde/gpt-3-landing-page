df3c84b93ce2891dc9310e7ace35d618ea084d7d
Holutunde <Holutunde99@gmail.com>
2023-02-25 16:36:19 +0100

    add call to action

--- src/component/cta/Cta.jsx
+++ src/component/cta/Cta.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import './cta.css'
 const Cta = () => {
   return (
-    <div>
-      CTA
+    <div className="cta_container">
+      <div className="cta_content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="cta_btn">
+        <button type="button">Get Started</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Cta
