615d17fb4c53ee76d24b357b741c0e24f2a88b74
Holutunde <Holutunde99@gmail.com>
2023-02-25 20:01:01 +0100

    added blog section

--- src/component/article/Article.jsx
+++ src/component/article/Article.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import './article.css'
+import React from 'react';
+import './article.css';
 
-const Article = () => {
-  return (
-    <div>
-      
+const Article = ({ imgUrl, date, text }) => (
+  <div className="blog_container_article">
+    <div className="blog_article_image">
+      <img src={imgUrl} alt="blog_image" />
     </div>
-  )
-}
+    <div className="blog_article_content">
+      <div>
+        <p>{date}</p>
+        <h3>{text}</h3>
+      </div>
+      <p>Read Full Article</p>
+    </div>
+  </div>
+);
 
-export default Article
+export default Article;
